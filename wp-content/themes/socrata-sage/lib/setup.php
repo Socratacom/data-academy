3c32a2c718260e8184d445bd42a2eb638954d0f1
Michael Church <michael@fishinglounge.com>
2017-06-09 15:32:27 -0400

    progress dev

--- wp-content/themes/socrata-sage/lib/setup.php
+++ wp-content/themes/socrata-sage/lib/setup.php
@@ -110,6 +110,6 @@ function assets() {
     wp_enqueue_script('comment-reply');
   }
 
-  wp_enqueue_script('sage/js', Assets\asset_path('scripts/main.js'), ['jquery'], '1.1', true);
+  wp_enqueue_script('sage/js', Assets\asset_path('scripts/main.js'), ['jquery'], '1.2', false);
 }
 add_action('wp_enqueue_scripts', __NAMESPACE__ . '\\assets', 100);
