bb6f5abc3eb6847b0b93a54d4886b560d3652f6a
Michael Church <michael@fishinglounge.com>
2017-06-20 11:00:06 -0400

    progress dev

--- wp-content/themes/socrata-sage/templates/header.php
+++ wp-content/themes/socrata-sage/templates/header.php
@@ -2,7 +2,7 @@
   <div class="container">
       <nav>
         <a class="logo color-logo" href="<?php echo home_url('/'); ?>"></a>
-        <a href="#form" class="btn btn-primary hidden-xs-down">Get Started</a>
+        <a href="#form" class="btn btn-primary hidden-xs-down">Get Information</a>
       </nav>
     </div>
 </header>
