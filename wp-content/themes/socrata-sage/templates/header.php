70ba567ef21b8853c2c7dda4eeb9b7d800d517a6
mike-church <mike.church@fishinglounge.com>
2017-10-13 11:11:30 -0400

    progress dev

--- wp-content/themes/socrata-sage/templates/header.php
+++ wp-content/themes/socrata-sage/templates/header.php
@@ -1,34 +1,9 @@
-
-
-    <!--<nav class="navbar">
-      <a class="logo color-logo" href="<?php echo home_url('/'); ?>"></a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggler" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-
-      <div class="collapse navbar-collapse" id="navbarToggler">
-        <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-          <li class="nav-item">
-            <a class="nav-link" href="/online-learning">Online</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="/in-person-learning">In-Person</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="/about-socrata-data-academy">About</a>
-          </li>
-        </ul>
-        <div class="my-2 my-lg-0"><a href="/contact" class="btn btn-primary hidden-xs-down">Learn More</a></div>
-
-      </div>
-    </nav>-->
-
 <nav id="siteNav" class="overlay">
   <a href="javascript:void(0)" class="icon-close" onclick="closeNav()"></a>
   <ul class="overlay-content">
     <li class="nav-item"><a href="/online-learning">Online Learning</a></li>
     <li class="nav-item"><a href="/in-person-learning">In-person Learning</a></li>
-    <li class="nav-item"><a href="/data-academy-as-a-service">Data Academy as a Service</a></li>
+    <li class="nav-item"><a href="/team-workshops">Team Workshops</a></li>
     <li class="nav-item"><a href="/contact">Contact Us</a></li>
   </ul>
 </nav>
@@ -39,7 +14,5 @@
       <span class="icon-menu"></span>
       <span class="text">Menu</span>
     </a>
-      <!--<span style="font-size:30px;cursor:pointer" onclick="openNav()">&#9776; open</span>-->
-
   </div>
 </header>
