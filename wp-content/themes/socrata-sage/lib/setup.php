d7049341819b90b101efebe9b7fd5d662780be35
Michael Church <michael@fishinglounge.com>
2017-10-16 16:21:01 -0400

    progress dev

--- wp-content/themes/socrata-sage/lib/setup.php
+++ wp-content/themes/socrata-sage/lib/setup.php
@@ -8,45 +8,47 @@ use Roots\Sage\Assets;
  * Theme setup
  */
 function setup() {
-  // Enable features from Soil when plugin is activated
-  // https://roots.io/plugins/soil/
-  add_theme_support('soil-clean-up');
-  add_theme_support('soil-nav-walker');
-  add_theme_support('soil-nice-search');
-  add_theme_support('soil-jquery-cdn');
-  add_theme_support('soil-relative-urls');
+	// Enable features from Soil when plugin is activated
+	// https://roots.io/plugins/soil/
+	add_theme_support('soil-clean-up');
+	add_theme_support('soil-nav-walker');
+	add_theme_support('soil-nice-search');
+	add_theme_support('soil-jquery-cdn');
+	add_theme_support('soil-relative-urls');
 
-  // Make theme available for translation
-  // Community translations can be found at https://github.com/roots/sage-translations
-  load_theme_textdomain('sage', get_template_directory() . '/lang');
+	// Make theme available for translation
+	// Community translations can be found at https://github.com/roots/sage-translations
+	load_theme_textdomain('sage', get_template_directory() . '/lang');
 
-  // Enable plugins to manage the document title
-  // http://codex.wordpress.org/Function_Reference/add_theme_support#Title_Tag
-  add_theme_support('title-tag');
+	// Enable plugins to manage the document title
+	// http://codex.wordpress.org/Function_Reference/add_theme_support#Title_Tag
+	add_theme_support('title-tag');
 
-  // Register wp_nav_menu() menus
-  // http://codex.wordpress.org/Function_Reference/register_nav_menus
-  register_nav_menus([
-    'primary_navigation' => __('Primary Navigation', 'sage')
-  ]);
+	// Register wp_nav_menu() menus
+	// http://codex.wordpress.org/Function_Reference/register_nav_menus
+	register_nav_menus([
+	'primary_navigation' => __('Primary Navigation', 'sage')
+	]);
 
-  // Enable post thumbnails
-  // http://codex.wordpress.org/Post_Thumbnails
-  // http://codex.wordpress.org/Function_Reference/set_post_thumbnail_size
-  // http://codex.wordpress.org/Function_Reference/add_image_size
-  add_theme_support('post-thumbnails');
+	// Enable post thumbnails
+	// http://codex.wordpress.org/Post_Thumbnails
+	// http://codex.wordpress.org/Function_Reference/set_post_thumbnail_size
+	// http://codex.wordpress.org/Function_Reference/add_image_size
+	add_theme_support('post-thumbnails');
+	set_post_thumbnail_size( 360, 180, array( 'center', 'center')  );
+	add_image_size( 'square', 200, 200, array( 'center', 'center'));  
 
-  // Enable post formats
-  // http://codex.wordpress.org/Post_Formats
-  add_theme_support('post-formats', ['aside', 'gallery', 'link', 'image', 'quote', 'video', 'audio']);
+	// Enable post formats
+	// http://codex.wordpress.org/Post_Formats
+	add_theme_support('post-formats', ['aside', 'gallery', 'link', 'image', 'quote', 'video', 'audio']);
 
-  // Enable HTML5 markup support
-  // http://codex.wordpress.org/Function_Reference/add_theme_support#HTML5
-  add_theme_support('html5', ['caption', 'comment-form', 'comment-list', 'gallery', 'search-form']);
+	// Enable HTML5 markup support
+	// http://codex.wordpress.org/Function_Reference/add_theme_support#HTML5
+	add_theme_support('html5', ['caption', 'comment-form', 'comment-list', 'gallery', 'search-form']);
 
-  // Use main stylesheet for visual editor
-  // To add custom styles edit /assets/styles/layouts/_tinymce.scss
-  add_editor_style(Assets\asset_path('styles/main.css'));
+	// Use main stylesheet for visual editor
+	// To add custom styles edit /assets/styles/layouts/_tinymce.scss
+	add_editor_style(Assets\asset_path('styles/main.css'));
 }
 add_action('after_setup_theme', __NAMESPACE__ . '\\setup');
 
@@ -54,23 +56,23 @@ add_action('after_setup_theme', __NAMESPACE__ . '\\setup');
  * Register sidebars
  */
 function widgets_init() {
-  register_sidebar([
-    'name'          => __('Primary', 'sage'),
-    'id'            => 'sidebar-primary',
-    'before_widget' => '<section class="widget %1$s %2$s">',
-    'after_widget'  => '</section>',
-    'before_title'  => '<h3>',
-    'after_title'   => '</h3>'
-  ]);
+	register_sidebar([
+		'name'          => __('Primary', 'sage'),
+		'id'            => 'sidebar-primary',
+		'before_widget' => '<section class="widget %1$s %2$s">',
+		'after_widget'  => '</section>',
+		'before_title'  => '<h3>',
+		'after_title'   => '</h3>'
+	]);
 
-  register_sidebar([
-    'name'          => __('Footer', 'sage'),
-    'id'            => 'sidebar-footer',
-    'before_widget' => '<section class="widget %1$s %2$s">',
-    'after_widget'  => '</section>',
-    'before_title'  => '<h3>',
-    'after_title'   => '</h3>'
-  ]);
+	register_sidebar([
+		'name'          => __('Footer', 'sage'),
+		'id'            => 'sidebar-footer',
+		'before_widget' => '<section class="widget %1$s %2$s">',
+		'after_widget'  => '</section>',
+		'before_title'  => '<h3>',
+		'after_title'   => '</h3>'
+	]);
 }
 add_action('widgets_init', __NAMESPACE__ . '\\widgets_init');
 
@@ -99,19 +101,19 @@ function display_sidebar() {
  * Theme assets
  */
 function assets() {
-  wp_enqueue_style('sage/css', Assets\asset_path('styles/main.css'), '', '1.7.6');  
-  wp_register_style( 'google-fonts', 'https://fonts.googleapis.com/css?family=Open+Sans:400,700,300,600', false, null);
-  wp_enqueue_style('google-fonts');
+	wp_enqueue_style('sage/css', Assets\asset_path('styles/main.css'), '', '1.7.6');  
+	wp_register_style( 'google-fonts', 'https://fonts.googleapis.com/css?family=Open+Sans:400,700,300,600', false, null);
+	wp_enqueue_style('google-fonts');
 
-  wp_deregister_script('jquery');
-  wp_enqueue_script('jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js', array(), null, true);
+	wp_deregister_script('jquery');
+	wp_enqueue_script('jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js', array(), null, true);
 
-  if (is_single() && comments_open() && get_option('thread_comments')) {
-    wp_enqueue_script('comment-reply');
-  }
+	if (is_single() && comments_open() && get_option('thread_comments')) {
+		wp_enqueue_script('comment-reply');
+	}
 
-  wp_enqueue_script('sage/js', Assets\asset_path('scripts/main.js'), ['jquery'], '1.4', false);  
-  wp_register_script('addthis', '//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-59480aa797904ef2', null, true);
-  wp_enqueue_script('addthis');
+	wp_enqueue_script('sage/js', Assets\asset_path('scripts/main.js'), ['jquery'], '1.4', false);  
+	wp_register_script('addthis', '//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-59480aa797904ef2', null, true);
+	wp_enqueue_script('addthis');
 }
 add_action('wp_enqueue_scripts', __NAMESPACE__ . '\\assets', 100);
