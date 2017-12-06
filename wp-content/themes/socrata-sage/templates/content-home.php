755b7df6e9596be72b7efbbb81798d615914029c
Michael Church <michael@fishinglounge.com>
2017-12-06 15:50:05 -0500

    progress dev

--- wp-content/themes/socrata-sage/templates/content-home.php
+++ wp-content/themes/socrata-sage/templates/content-home.php
@@ -3,6 +3,34 @@
 		<div class="row">
 			<div class="col-sm-12">
 				<h1>Socrata Data Academy</h1>
+
+<?php echo do_shortcode('[stat-totals]');?>
+
+				<div id="myDiv"></div>
+
+<script>
+var data = [{
+x: ['giraffes', 'orangutans', 'monkeys'],
+y: [20, 14, 23], 
+name: 'SF Zoo', 
+type: 'bar',
+marker: {color: '#19d3f3'}
+}, {
+x: ['giraffes', 'orangutans', 'monkeys'],
+y: [12, 18, 29], 
+name: 'LA Zoo', 
+type: 'bar',
+marker: {color: '#ab63fa'} 
+}];
+
+var layout = {
+plot_bgcolor: '#F5F7FA',
+paper_bgcolor: '#F5F7FA',
+width: 500
+};
+
+Plotly.newPlot('myDiv', data, layout);
+</script>
 			</div>
 		</div>
 	</div>
--- wp-content/themes/socrata-sage/templates/footer.php
+++ wp-content/themes/socrata-sage/templates/footer.php
@@ -1,11 +1,13 @@
 <footer class="section-padding">
   <div class="container">
   	<div class="row">
-  		<div class="col-sm-12">
-  			<ul class="copyright">
-  				<li><a href="https://socrata.com" target="_blank" class="socrata-logo mr-2"></a></li>
-  				<li><span style="font-size:12px;">&copy; <?php echo date("Y");?> Socrata. All rights reserved.</span></li>
-    		</ul>
+    	<div class="col-sm-6 order-sm-2">
+    		<h4 class="font-light">Subscribe to our newsletter</h4>
+    		<iframe id="formIframe" style="width: 100%; border: 0;" src="https://go.pardot.com/l/303201/2017-12-06/tyrc" width="300" height="150" scrolling="no"></iframe>
+    	</div>
+  		<div class="col-sm-6 order-sm-1">
+  			<p class="mb-0"><a href="https://socrata.com" target="_blank" class="socrata-logo mr-2"></a></p>
+  			<p><span style="font-size:12px;">&copy; <?php echo date("Y");?> Socrata. All rights reserved.</span></p>
     	</div>
     </div>
   </div>
@@ -19,3 +21,4 @@ function closeNav() {
   document.getElementById("siteNav").style.height = "0%";
 }
 </script>
+<script>iFrameResize({log:true}, '#formIframe')</script>
