4507c2b7dc9b421c063f42986e927e01ff783dd0
Michael Church <michael@fishinglounge.com>
2017-12-07 17:42:21 -0500

    progress dev

--- wp-content/themes/socrata-sage/templates/footer.php
+++ wp-content/themes/socrata-sage/templates/footer.php
@@ -2,7 +2,8 @@
   <div class="container">
   	<div class="row">
     	<div class="col-sm-6 order-sm-2">
-    		<h4 class="font-light">Subscribe to our newsletter</h4>
+    		<h4 class="font-light">Subscribe to the Socrata Data Academy Newsletter</h4>
+    		<p>The Socrata Data Academy newsletter is a bi-monthly email communication that provides curated news, updates, notifications, and best practices about data literacy and analysis in government.</p>
     		<iframe id="formIframe" style="width: 100%; border: 0;" src="https://go.pardot.com/l/303201/2017-12-06/tyrc" width="300" height="150" scrolling="no"></iframe>
     	</div>
   		<div class="col-sm-6 order-sm-1">
